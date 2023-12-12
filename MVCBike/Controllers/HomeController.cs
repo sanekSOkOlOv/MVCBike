4078c306846f4b46308d1eac37b06021480ec4c1
sanekSOkOlOv <oleksandr.sokolov@nure.ua>
2023-12-12 11:32:56 +0200

    Добавьте файлы проекта.

---
+++ MVCBike/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCBike.Models;
+using System.Diagnostics;
+using System.Text.Json;
+using System.IO;
+
+namespace MVCBike.Controllers
+{
+    public class HomeController : Controller
+    {
+        private const string FilePath = "bikes.json";
+
+        public IActionResult Index()
+        {
+            List<Bike> bikes = LoadBikes();
+            return View(bikes);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Bike bike)
+        {
+            List<Bike> bikes = LoadBikes();
+            bike.Id = bikes.Count + 1;
+            bikes.Add(bike);
+            SaveBikes(bikes);
+
+            return RedirectToAction("Index");
+        }
+
+        private List<Bike> LoadBikes()
+        {
+            if (System.IO.File.Exists(FilePath))
+            {
+                string json = System.IO.File.ReadAllText(FilePath);
+                return JsonSerializer.Deserialize<List<Bike>>(json);
+            }
+            return new List<Bike>();
+        }
+
+        private void SaveBikes(List<Bike> bikes)
+        {
+            string json = JsonSerializer.Serialize(bikes);
+            System.IO.File.WriteAllText(FilePath, json);
+        }
+    }
+}
---
+++ MVCBike/Models/Bike.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCBike.Models
+{
+    public class Bike
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Rating { get; set; }
+    }
+}
